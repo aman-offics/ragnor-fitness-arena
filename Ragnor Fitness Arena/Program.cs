936908c426ca94be2fa7863f2e48ed96022794ba
Mohammed Aman <mohammedaman3160@gmail.com>
2026-04-03 11:55:05 +0530

    Fixed production error page

--- Ragnor Fitness Arena/Program.cs
+++ Ragnor Fitness Arena/Program.cs
@@ -8,12 +8,24 @@ builder.Services.AddSession();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+builder.Services.AddAntiforgery(options =>
+{
+    options.SuppressXFrameOptionsHeader = true;
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
